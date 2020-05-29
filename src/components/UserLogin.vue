1108e0feed219928d204ed995e425b5d7821a88a
Yogendra Prasad <yogendra.prasad@srijan.net>
2020-05-29 15:10:30 +0530

    Added component

---
+++ src/components/UserLogin.vue
@@ -0,0 +1,57 @@
+<template>
+    <GoogleLogin :params="params" :renderParams="renderParams" :onSuccess="onSuccess" :onFailure="onFailure"></GoogleLogin>
+</template>
+
+<script>
+import GoogleLogin from "vue-google-login";
+
+export default {
+  name: "UserLogin",
+  components: {
+    GoogleLogin
+  },
+
+  data() {
+    return {
+      // client_id is the only required property but you can add several more params, full list down bellow on the Auth api section
+      params: {
+        client_id: "712428694771-kmku1htdf3287e3cc34i9aalkgujqns6.apps.googleusercontent.com"
+      },
+      // only needed if you want to render the button with the google ui
+      renderParams: {
+        width: 250,
+        height: 50,
+        longtitle: true
+      }
+    };
+  },
+
+   methods: {
+        onSuccess(googleUser) {
+            console.log(googleUser);
+ 
+            // This only gets the user information: id, name, imageUrl and email
+            console.log(googleUser.getBasicProfile());
+        },
+        onFailure(googleUser) {
+            console.log(googleUser);
+ 
+            // This only gets the user information: id, name, imageUrl and email
+            console.log(googleUser.getBasicProfile());
+        }
+    }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+.form-wrapper {
+  width: 100%;
+  margin: auto;
+}
+
+.text-field {
+  padding: 5px;
+  margin: 10px;
+}
+</style>
