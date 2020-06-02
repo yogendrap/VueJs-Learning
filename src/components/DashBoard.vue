d0ee306835714ad20a109d97860a2bf7640ec9cf
Yogendra Prasad <yogendra.prasad@srijan.net>
2020-06-02 13:23:31 +0530

    Added bootstarp

--- src/components/DashBoard.vue
+++ src/components/DashBoard.vue
@@ -1,7 +1,12 @@
 <template>
 <div>
   <AppNavigation></AppNavigation>
-
+  <div class="alert alert-success alert-dismissible fade show" role="alert">
+  With Bootstrap!
+  <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+    <span aria-hidden="true">&times;</span>
+  </button>
+</div>
    Welcome {{userInfo.name}}
   <br>
    Email {{userInfo.mail}}
--- src/main.js
+++ src/main.js
@@ -2,6 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router/index.js'
 
+import 'bootstrap'; 
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 Vue.config.productionTip = false
 
 new Vue({
